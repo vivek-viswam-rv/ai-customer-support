0c8319851fd2faaa0ff501fa5ab5fd378687d5c9
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-01-19 05:54:24 -0500

    Added Redis to database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+from redis import Redis
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("sqlite:///database.db")
+load_dotenv()
 
+engine = create_engine(os.getenv("DATABASE_URL"))
 Base = declarative_base()
-
 Session = sessionmaker(bind=engine)
 session = Session()
+
+redis = Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), db=0)
