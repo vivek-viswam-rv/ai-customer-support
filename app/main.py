ef0d6602e16606c9d5500df729aa4ec24d5a2fb8
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-01-19 05:55:40 -0500

    Added rate limiter to main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
 from .database import engine, Base
-from .middlewares import authentication_middleware
+from .middlewares import authentication_middleware, rate_limiter_middleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.middleware("http")(rate_limiter_middleware)
 app.middleware("http")(authentication_middleware)
