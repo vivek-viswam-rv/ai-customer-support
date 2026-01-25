ac89537582d5441c10600369fc935235e327ecb1
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-01-25 09:57:36 -0500

    Changed Base model id type to UUID

--- app/models/base.py
+++ app/models/base.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from uuid import uuid4
 
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, UUID, DateTime
 
 from app.database import Base as DBBase
 
 class Base(DBBase):
     __abstract__ = True
-    id = Column(String, default=uuid4, primary_key=True)
+    id = Column(UUID, default=uuid4, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
