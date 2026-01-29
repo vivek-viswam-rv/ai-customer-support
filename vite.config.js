863d7f9ee7f10d98770c3172a55927d4f5b72a99
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-01-29 13:39:08 -0500

    Set up jsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import jsconfigPaths from "vite-jsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   root: "./ui",
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), jsconfigPaths()],
 });
