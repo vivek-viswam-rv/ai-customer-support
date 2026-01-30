b430709653c657a037108d2263b5aaf7f4ed477e
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-01-30 06:00:24 -0500

    Added home to login redirection

--- ui/src/components/routeConstants.js
+++ ui/src/components/routeConstants.js
@@ -2,9 +2,16 @@
 import NotFound from "./commons/NotFound";
 import Login from "./Login";
 
+import { redirect } from "react-router-dom";
+
+export const HOME_ROUTE = "/";
 export const LOGIN_ROUTE = "/login";
 
 export const ROUTES = [
+  {
+    path: HOME_ROUTE,
+    loader: () => redirect(LOGIN_ROUTE),
+  },
   {
     path: LOGIN_ROUTE,
     element: Login(),
