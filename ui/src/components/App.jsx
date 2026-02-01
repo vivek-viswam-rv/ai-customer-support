adaade467a566db16204658a58a66eb5115ba163
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-01 12:19:49 -0500

    Updated redirection to Login page if not logged in

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ROUTES } from "./routeConstants";
+
+import { LOGIN_ROUTE, ROUTES } from "./routeConstants";
+import { getFromLocalStorage } from "utils/storage";
 
 const router = createBrowserRouter(ROUTES);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  const pathname = window.location.pathname;
+  const isLoggedIn = Boolean(getFromLocalStorage("apiKey"));
+
+  if (!isLoggedIn && !pathname.includes(LOGIN_ROUTE)) {
+    router.navigate(LOGIN_ROUTE);
+  }
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- ui/src/components/routeConstants.jsx
+++ ui/src/components/routeConstants.jsx
@@ -3,14 +3,20 @@ import NotFound from "./commons/NotFound";
 
 import { redirect } from "react-router-dom";
 import Ticket from "./Ticket";
+import Login from "./Login";
 
 export const HOME_ROUTE = "/";
 export const TICKET_ROUTE = "/ticket";
+export const LOGIN_ROUTE = "/login";
 
 export const ROUTES = [
   {
     path: HOME_ROUTE,
     loader: () => redirect(TICKET_ROUTE),
+  },
+  {
+    path: LOGIN_ROUTE,
+    element: <Login />,
   },
   {
     path: TICKET_ROUTE,
