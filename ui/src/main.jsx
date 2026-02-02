6d2037a89774a77a6f8516c117ea85e7c7fa52ef
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-02 05:40:00 -0500

    Integrated TanStack Query into the application

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
-import App from "./components/App.jsx";
+import App from "components/App.jsx";
 import { Toaster } from "shadcn/sonner.jsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "utils/queryClient.js";
 
 createRoot(document.getElementById("root")).render(
-  <>
+  <QueryClientProvider client={queryClient}>
     <App />
     <Toaster position="bottom-left" />
-  </>
+  </QueryClientProvider>
 );
