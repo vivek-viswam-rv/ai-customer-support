17a1648143021ed2ad0ab8565f96b7b121220d83
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-03 10:58:15 -0500

    Updated database file and created models

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String
+
+from .database import Base, gen_uuid
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, index=True, default=gen_uuid)
+    email: Mapped[str] = mapped_column(String(120), unique=True, index=True, nullable=False)
+    api_key: Mapped[str] = mapped_column(String(36), unique=True, index=True, nullable=False, default=gen_uuid)
+
+    tickets: Mapped[list[Ticket]] = relationship(back_populates="author")
+
+class Ticket(Base):
+    __tablename__ = "tickets"
+
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, index=True, default=gen_uuid)
+    description: Mapped[str] = mapped_column(String(1000), nullable=False)
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    author: Mapped[User] = relationship(back_populates="tickets")
