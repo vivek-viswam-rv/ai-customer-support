c45fb300ab2cad148cc6b4474cc8cb705304dc6a
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-03 14:14:52 -0500

    Updated App.jsx to register intercepts and header setting logic

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { LOGIN_ROUTE, ROUTES } from "./routeConstants";
 import { getFromLocalStorage } from "utils/storage";
+import { registerIntercepts, setAuthHeaders } from "apis/axois";
 
 const router = createBrowserRouter(ROUTES);
 
 function App() {
   const pathname = window.location.pathname;
   const isLoggedIn = Boolean(getFromLocalStorage("apiKey"));
+
+  useEffect(() => {
+    registerIntercepts();
+    setAuthHeaders();
+  },[]);
 
   if (!isLoggedIn && !pathname.includes(LOGIN_ROUTE)) {
     router.navigate(LOGIN_ROUTE);
