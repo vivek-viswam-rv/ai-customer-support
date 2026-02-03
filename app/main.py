4a75e77318565be778d9bf3eb61caa924bb43c89
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-03 14:08:47 -0500

    Added CORS middleware to main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import tickets_router, users_router
 from .database import engine, Base
@@ -6,6 +7,14 @@ from .database import engine, Base
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tickets_router, prefix="/api")
 app.include_router(users_router, prefix="/api")
