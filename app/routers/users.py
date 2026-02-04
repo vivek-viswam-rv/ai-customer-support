85f71be413adcccce1ecf83281f396b87c7c0527
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-04 06:53:45 -0500

    Updated users router to handle existing user case

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.auth import hash_password
@@ -13,6 +14,11 @@ router = APIRouter(prefix="/users")
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+    does_user_exist = db.execute(select(User).where(User.email == user.email).limit(1)).first()
+
+    if does_user_exist:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with this email already exists!")
+
     password_hash = hash_password(user.password)
     new_user = User(email=user.email, password_hash=password_hash)
 
@@ -23,7 +29,7 @@ def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
 
 @router.post("/signin", status_code=status.HTTP_200_OK)
 def signin_user(user_details: UserSignIn, db: Annotated[Session, Depends(get_db)]):
-    user = db.query(User).filter(User.email == user_details.email).first()
+    user = db.scalars(select(User).where(User.email == user_details.email).limit(1)).first()
     if not user or not verify_password(user_details.password, user.password_hash):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password!")
 
