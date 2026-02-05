58ad1089dfcb2f0aee45eaa5cff0ebd987f5a705
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-05 11:21:56 -0500

    Added Order and OrderHistory models

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class User(BaseModel):
     api_key: Mapped[str] = mapped_column(String(36), unique=True, index=True, nullable=False, default=gen_uuid)
 
     tickets: Mapped[list[Ticket]] = relationship(back_populates="author")
+    orders: Mapped[list[Order]] = relationship(back_populates="buyer")
 
 class Ticket(BaseModel):
     __tablename__ = "tickets"
@@ -31,3 +32,40 @@ class Ticket(BaseModel):
     )
 
     author: Mapped[User] = relationship(back_populates="tickets")
+
+class Order(BaseModel):
+    __tablename__ = "orders"
+
+    product_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    status: Mapped[str] = mapped_column(String(60), nullable=False, default="Delivered")
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    buyer: Mapped[User] = relationship(back_populates="orders")
+    history: Mapped[list[OrderHistory]] = relationship(back_populates="order", cascade="all, delete-orphan")
+
+class OrderHistory(BaseModel):
+    __tablename__ = "order_history"
+
+    status: Mapped[str] = mapped_column(String(60), nullable=False, default="Delivered")
+    order_id: Mapped[str] = mapped_column(
+        ForeignKey("orders.id"),
+        nullable=False
+    )
+
+    order: Mapped[Order] = relationship(back_populates="history")
+
+def log_order_status_change(__, connection, target):
+    history_table = OrderHistory.__table__
+    connection.execute(
+        history_table.insert().values(
+            order_id=target.id,
+            status=target.status,
+            created_at=func.now()
+        )
+    )
+
+event.listen(Order, "after_update", log_order_status_change)
+event.listen(Order, "after_insert", log_order_status_change)
