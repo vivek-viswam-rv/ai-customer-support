0bd6f4bb2e8e710f8fdb4e837d3ba14626392d0b
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-05 11:23:38 -0500

    Updated user signup router to populate order data and defined schemas

--- app/routers/users.py
+++ app/routers/users.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import Session
 
 from app.auth import hash_password
 from app.database import get_db
-from app.models import User
-from app.schemas import UserCreate, UserSignIn, UserSignInResponse
+from app.models import User, Order
+from app.schemas import OrderStatus, UserCreate, UserSignIn, UserSignInResponse
 from app.auth import verify_password
 
 router = APIRouter(prefix="/users")
@@ -23,6 +23,8 @@ def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
     new_user = User(email=user.email, password_hash=password_hash)
 
     db.add(new_user)
+    db.flush()
+    populate_orders(new_user, db)
     db.commit()
 
     return {"message": "Sign up successful!"}
@@ -34,3 +36,10 @@ def signin_user(user_details: UserSignIn, db: Annotated[Session, Depends(get_db)
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password!")
 
     return UserSignInResponse(email=user.email, api_key=user.api_key)
+
+def populate_orders(user: User, db: Session):
+    db.add_all([
+        Order(user_id=user.id, product_name="Wireless Mouse", status=OrderStatus(status="Delivered").status),
+        Order(user_id=user.id, product_name="Bluetooth Headphones", status=OrderStatus(status="Refunded").status),
+        Order(user_id=user.id, product_name="USB-C Charger", status=OrderStatus(status="Returned").status),
+    ])
