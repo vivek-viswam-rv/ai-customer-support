0d79456726620d8c6354d62dcf75f18cbed168aa
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-05 18:18:08 -0500

    Renamed api_authenticaition to authentication

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -1,3 +1,2 @@
-from .api_authentication import api_authentication
-from .api_authentication import stream_authentication
+from .authentication import api_authentication, stream_authentication
 from .rate_limiter import rate_limiter
