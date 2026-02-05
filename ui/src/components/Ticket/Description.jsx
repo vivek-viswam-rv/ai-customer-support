bc5e1a71c958113fd004c29b2faa511c27ad0d46
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2026-02-05 14:27:15 -0500

    Updated Description

--- ui/src/components/Ticket/Description.jsx
+++ ui/src/components/Ticket/Description.jsx
@@ -9,11 +9,14 @@ const Description = () => (
         issue. Below is a list of your past purchases:
         <br />
       </p>
-      <ol className="pl-5 list-decimal">
-        <li>Wireless Mouse</li>
-        <li>Bluetooth Headphones</li>
-        <li>USB-C Charger</li>
+      <ol className="pl-5 list-decimal mb-1">
+        <li>Wireless Mouse - Delivered</li>
+        <li>Bluetooth Headphones - Refunded</li>
+        <li>USB-C Charger - Returned</li>
       </ol>
+      You may ask about order status, request return and refund and the
+      responses will be based on the return policy and database records of these
+      orders. The agent has the ability to update those records as needed.
     </div>
   </div>
 );
